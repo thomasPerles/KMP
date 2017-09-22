d1a82edd6359c16eda0fc3fc40a1bf133183f52c
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-09-22 09:31:59 +0200

    Terminal prompt

--- Project KMP/src/terminal/Terminal.java
+++ Project KMP/src/terminal/Terminal.java
@@ -4,25 +4,27 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Terminal {
+public class Terminal implements Runnable {
 	
-	InputStream in;
-	PrintStream out;
-	Scanner scanner;
+	private InputStream in;
+	private PrintStream out;
+	private Scanner scanner;
+	private boolean hasQuit;
 	
 	public Terminal(InputStream in, PrintStream out) {
 		this.in = in;
 		this.out = out;
 		scanner = new Scanner(in);
+		hasQuit = false;
 	}
 	
 	public void prompt() {
-		out.println("\n$ ");
+		out.print(System.lineSeparator() + "> ");
 	}
 	
 	public String consume() {
-		String typed = scanner.nextLine();
-		return typed;
+		String consumed = scanner.nextLine();
+		return consumed;
 	}
 	
 	public void interpret(String line) {
@@ -31,5 +33,17 @@ public class Terminal {
 
 	private void displayOptions(String line) {
 		
+	}
+	
+	private void quit() {
+		hasQuit = true;
+	}
+
+	@Override
+	public void run() {
+		while(!hasQuit) {
+			prompt();
+			System.out.println("Command consumed: " + consume());
+		}
 	}
 }
