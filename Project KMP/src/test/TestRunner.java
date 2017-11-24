894dfa84e59b263b00cd79b0495b7f9321ed5f6c
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-11-24 16:37:11 +0100

    Transitive test

--- Project KMP/src/test/TestRunner.java
+++ Project KMP/src/test/TestRunner.java
@@ -23,10 +23,28 @@ public class TestRunner {
 	}
 	
 	@Test
-	public void newStatementTest() {
+	public void symmetricTest() {
 		db.setRelation_symmetric("isMarried");
 		db.newStatement(new String[]{"Laurent", "isMarried", "Sophie"});
 		assertTrue(db.getDBKObject().size() == 3 && db.getDBTriple().size() == 2);
+	}
+	
+	@Test
+	public void transitiveTest() {
+		db.setRelation_transitive("hasSibling");
+		db.setRelation_symmetric("hasSibling");
+		db.newStatement(new String[] {"Alice", "hasSibling", "Bob"});
+		assertTrue(db.getDBKObject().size() == 3 && db.getDBTriple().size() == 2);
+		db.newStatement(new String[] {"Bob", "hasSibling", "Charles"});
+		assertTrue(db.getDBKObject().size() == 4 && db.getDBTriple().size() == 6);
+		/*
+		 * ["Alice", "hasSibling", "Bob"]
+		 * ["Alice", "hasSibling", "Charles"]
+		 * ["Bob", "hasSibling", "Charles"]
+		 * ["Bob", "hasSibling", "Alice"]
+		 * ["Charles", "hasSibling", "Alice"]
+		 * ["Charles", "hasSibling", "Bob"]
+		 */
 	}
 
 }
