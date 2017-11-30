fe90c485e931ad11ee18f46f30c53c9782bdb43b
Thomas Lecointre <thomas.lecointre1@gmail.com>
2017-11-30 18:25:26 +0100

    Formatting

--- Project KMP/src/terminal/Terminal.java
+++ Project KMP/src/terminal/Terminal.java
@@ -8,13 +8,13 @@ import language.IllegalWordCountException;
 import language.StringProcessor;
 
 public class Terminal implements Runnable {
-	
+
 	private InputStream in;
 	private PrintStream out;
 	private Scanner scanner;
 	private StringProcessor sv;
 	private boolean hasQuit;
-	
+
 	public Terminal(InputStream in, PrintStream out, StringProcessor sv) {
 		this.in = in;
 		this.out = out;
@@ -23,53 +23,54 @@ public class Terminal implements Runnable {
 		sv.setTerminal(this);
 		hasQuit = false;
 	}
-	
+
 	public void prompt() {
 		out.print(System.lineSeparator() + "> ");
 	}
-	
+
 	public String[] consume() {
 		String[] consumed = scanner.nextLine().split(" ");
 		return consumed;
 	}
-	
+
 	public void println(String token) {
 		out.println(token);
 	}
-	
+
 	public void println(String[] tokens) {
 		out.println();
-		for(String token : tokens) {
+		for (String token : tokens) {
 			out.print(token + ' ');
 		}
 	}
-	
+
 	public void print(String token) {
 		out.print(token);
 	}
-	
+
 	public void print(int token) {
 		out.print(token);
 	}
-	
+
 	public void print(char cToken) {
 		out.print(cToken);
 	}
-	
+
 	private void quit() {
 		hasQuit = true;
 	}
 
 	@Override
 	public void run() {
-		
-		while(!hasQuit) {
+
+		while (!hasQuit) {
 			prompt();
 			String tokens[] = consume();
 			try {
 				sv.process(tokens);
 			} catch (IllegalWordCountException e) {
-				System.err.println(e.getMessage());;
+				System.err.println(e.getMessage());
+				;
 			}
 		}
 	}
